0cf0309c6c428d318dcddf5f37f69eb9a27abd44
Abd El-Rahman Fawzy <102333002+AbdEl-Rahman-Fawzy@users.noreply.github.com>
2024-05-16 11:09:07 +0300

    Delete assignment directory

--- assignment/footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-    <h4>&copy; 2024 <span>FCAI-CU</span>. All rights reserved</h4>
-
-</footer> 
-
-<div id="errorMessages"></div> <!-- Error messages container -->
-<script src="javascript/clientValidations.js"></script>
-</body>
-</html>
